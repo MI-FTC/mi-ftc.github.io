7d34165095efe9a4675776887a56e54f7e7b20f6
MediumAutomaton <michaelr@wordenhome.net>
2023-08-23 15:56:44 -0500

    8-23-23 midday - need to test something with the published site

--- download/boilerplate/BoilerPlate_LinearOpMode.java
+++ download/boilerplate/BoilerPlate_LinearOpMode.java
@@ -4,16 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 //or @Autonomous
-@TeleOp(name="", group="")
+//Change these arguments to change the appearance in the Driver Station
+@TeleOp(name="LinearOpMode Boiler Plate", group="Tutorial")
 public class BoilerPlate_LinearOpMode extends LinearOpMode {
+
+    //Declare variables (more specifically class members) here
 
     @Override
     public void runOpMode() {
-        //initialization
+      
+        //Initialize things here
+        
         waitForStart();
+        
+        //Code here will run only once when Start is pressed
 
         while (opModeIsActive()) {
+          
             //loop until Stop is pressed
+            
+            telemetry.update();
         }
+        
+        //Put anything here that needs to run once when Stop is pressed
+        
     }
 }
--- download/boilerplate/BoilerPlate_OpMode.java
+++ download/boilerplate/BoilerPlate_OpMode.java
@@ -4,31 +4,44 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 //or @Autonomous
-@TeleOp(name="", group="")
+//Change these arguments to change the appearance in the Driver Station
+@TeleOp(name="OpMode Boiler Plate", group="Tutorial")
 public class BoilerPlate_OpMode extends OpMode {
+  
+    //Declare variables (more specifically class members) here
 
     @Override
     public void init() {
-        //initialization
+      
+        //Initialize things here
+        
     }
 
     @Override
     public void init_loop() {
+      
         //loop until Start is pressed
+        
     }
 
     @Override
     public void start() {
+      
         //run once when Start is pressed
+        
     }
 
     @Override
     public void loop() {
+      
         //loop until Stop is pressed
+        
     }
 
     @Override
     public void stop() {
+      
         //run once when Stop is pressed
+        
     }
 }
