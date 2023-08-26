3556d14ca8a55487d853b9c000dfe4dec557d3cf
MediumAutomaton <michaelr@wordenhome.net>
2023-08-25 20:27:49 -0500

    8-25-23 nightly - finished theming support

--- internal/lib/theming.js
+++ internal/lib/theming.js
@@ -1,95 +1,278 @@
-function setTheme(theme) {
-  localStorage.setItem("theme", theme);
-  updateSetThemeDisplay();
-}
-
-function saveCustomTheme() {
-  var dictToStore = {
-    "background": document.getElementById("backgroundInput").value,
-    "navtable": document.getElementById("navtableInput").value,
-    "link": document.getElementById("linkInput").value,
-    "text": document.getElementById("textInput").value
-  };
-  localStorage.setItem("themeCustom", JSON.stringify(dictToStore));
-}
-
-function populateCustomThemeFields() {
-  var themeCustom = JSON.parse(localStorage.getItem("themeCustom"));
-  document.getElementById("backgroundInput").value = themeCustom["background"];
-  document.getElementById("navtableInput").value = themeCustom["navtable"];
-  document.getElementById("linkInput").value = themeCustom["link"];
-  document.getElementById("textInput").value = themeCustom["text"];
-}
-
-
-function getTextTags() {
-  var tagsToGet = ["p", "li", "h1", "h2", "h3", "h4", "h5", "h6", "b", "i", "strong", "em", "code"];
-  var returnArray = [];
-  tagsToGet.forEach(function(item) {
-    returnArray.push.apply(returnArray, Array.from(document.getElementsByTagName(item)));
-  });
-  return returnArray;
-}
-
-
-var themeLight = {
+var themeFields = [
+  "background",
+  "navtable",
+  "link",
+  "text",
+  "specific",
+  "keyword",
+  "annotation",
+  "comment",
+  "string",
+  "field",
+  "number"
+];
+  
+  var builtinThemeLight = {
   "background": "white",
   "navtable": "blue",
   "link": "blue",
-  "text": "black"
+  "text": "black",
+  "specific": "green",
+  "keyword": "blue",
+  "annotation": "goldenrod",
+  "comment": "gray",
+  "string": "green",
+  "field": "indigo",
+  "number": "blue"
 };
 
-var themeDefault = {
+var builtinThemeDefault = {
   "background": "lightgray",
   "navtable": "blue",
   "link": "blue",
-  "text": "black"
+  "text": "black",
+  "specific": "green",
+  "keyword": "blue",
+  "annotation": "goldenrod",
+  "comment": "gray",
+  "string": "green",
+  "field": "indigo",
+  "number": "blue"
 };
 
-var themeDark = {
+var builtinThemeDark = {
   "background": "#202020",
   "navtable": "darkgreen",
   "link": "green",
-  "text": "lightgray"
+  "text": "lightgray",
+  "specific": "green",
+  "keyword": "darkorange",
+  "annotation": "goldenrod",
+  "comment": "gray",
+  "string": "green",
+  "field": "indigo",
+  "number": "blue"
 };
 
-
-function applyTheme(theme) {
-  document.body.setAttribute("style", "background-color: " + theme["background"]);
-  var aTags = Array.from(document.getElementsByTagName("a"));
-  aTags.forEach(function(item) {
-    item.setAttribute("style", "color: " + theme["link"]);
-  });
-  
-  var textTags = getTextTags();
-  textTags.forEach(function(item) {
-    item.setAttribute("style", "color: " + theme["text"]);
-  });
-  
-  document.getElementById("navtable").setAttribute("style", "border: 1px solid " + theme["navtable"] + ";");
-  document.getElementById("navth").setAttribute("style", "background-color: " + theme["navtable"] + ";");
-  Array.from(document.getElementsByClassName("navtd")).forEach(function(item) {
-    item.setAttribute("style", "border: 1px solid " + theme["navtable"] + "; color: " + theme["text"] + ";");
-  });
+var flag = localStorage.getItem("themeManRan");
+if (flag == undefined || flag == false) {
+  restoreBuiltInThemes();
+  localStorage.setItem("themeCustom", JSON.stringify({}));
+  localStorage.setItem("themeManRan", true);
 }
 
 
-function applySetTheme() {
-  var setTheme = localStorage.getItem("theme");
-  console.log(setTheme);
-  if (setTheme == "light") {
-    applyTheme(themeLight);
-  } else if (setTheme == "default") {
-    applyTheme(themeDefault);
-  } else if (setTheme == "dark") {
-    applyTheme(themeDark);
-  } else if (setTheme == "custom") {
-    applyTheme(JSON.parse(localStorage.getItem("themeCustom")));
+/* Functions Used by the Theme Manager */
+function initThemeMan() {
+  applySetTheme();
+  updateSetThemeDisplay();
+  populateCustomThemeFields(getSetTheme());
+}
+
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+  updateSetThemeDisplay();
+  populateCustomThemeFields(getSetTheme());
+}
+
+function populateCustomThemeFields(theme) {
+  themeFields.forEach(function(item) {
+    document.getElementById(item + "Input").value = theme[item];
+  });
+}
+
+function saveSetTheme() {
+  var dictToStore = {};
+  for (item of themeFields) {
+    dictToStore[item] = document.getElementById(item + "Input").value;
   }
+  var themeStr = localStorage.getItem("theme");
+  localStorage.setItem("theme" + themeStr[0].toUpperCase() + themeStr.slice(1), JSON.stringify(dictToStore));
 }
-
 
 function updateSetThemeDisplay() {
   var setThemeDisplay = document.getElementById("setThemeDisplay");
   setThemeDisplay.innerHTML = localStorage.getItem("theme");
+}
+
+function restoreBuiltinThemes() {
+  localStorage.setItem("themeLight", JSON.stringify(builtinThemeLight));
+  localStorage.setItem("themeDefault", JSON.stringify(builtinThemeDefault));
+  localStorage.setItem("themeDark", JSON.stringify(builtinThemeDark));
+}
+
+
+/* Set Theme getter/setter */
+function getSetTheme() {
+  var setTheme = localStorage.getItem("theme");
+  if (setTheme == "light") {
+    return JSON.parse(localStorage.getItem("themeLight"));
+  } else if (setTheme == "default") {
+    return JSON.parse(localStorage.getItem("themeDefault"));
+  } else if (setTheme == "dark") {
+    return JSON.parse(localStorage.getItem("themeDark"));
+  } else if (setTheme == "custom") {
+    return JSON.parse(localStorage.getItem("themeCustom"));
+  } else {
+    return "noTheme";
+  }
+}
+
+function applySetTheme() {
+  applyTheme(getSetTheme());
+}
+
+
+/* Get Tags/Classes */
+function getTextTags() {
+  var tagsToGet = ["p", "li", "h1", "h2", "h3", "h4", "h5", "h6", "b", "i", "strong", "em", "code"];
+  var returnArray = [];
+  tagsToGet.forEach(function(item) {
+    try {
+      returnArray.push.apply(returnArray, Array.from(document.getElementsByTagName(item)));
+      /* Join returnArray and array of all desired tags */
+    } catch(error) {
+      console.log("Couldn't get '" + item.toString() + "'");
+      console.log(error);
+    }
+  });
+  return returnArray;
+}
+
+/*function getColorizationTags() {
+  var classesToGet = ["specific", "keyword", "field", "annotation", "string", "comment", "number"];
+  var returnArray = [];
+  classesToGet.forEach(function(item) {
+    try {
+      returnArray.push.apply(returnArray, Array.from(document.getElementsByClassName(item)));
+    } catch(error) {
+      console.log("Couldn't get '" + item.toString() + "'");
+      console.log(error);
+    }
+  });
+  return returnArray;
+}*/
+
+function getLinkTags() {
+  try {
+    var returnArray = Array.from(document.getElementsByTagName("a"));
+    return returnArray;
+  } catch(error) {
+    console.log("Couldn't get link tags.");
+    console.log(error);
+  }
+}
+
+
+/* Apply Theme */
+function applyText(theme) {
+  var textTags = getTextTags();
+  for (item of textTags) {
+    try {
+      item.setAttribute("style", "color: " + theme["text"] + ";");
+    } catch(error) {
+      console.log("Couldn't apply theme property 'text' to '" + item.toString() + "'");
+      console.log(error);
+    }
+  }
+}
+
+function applyBackground(theme) {
+  try {
+    document.body.setAttribute("style", "background-color: " + theme["background"]);
+  } catch(error) {
+    console.log("Couldn't apply theme property 'background'");
+    console.log(error);
+  }
+}
+
+function applyLinks(theme) {
+  var linkTags = getLinkTags();
+  for (item of linkTags) {
+    try {
+      item.setAttribute("style", "color: " + theme["link"] + ";");
+    } catch(error) {
+      console.log("Couldn't apply theme property 'link' to '" + item.toString() + "'");
+      console.log(error);
+    }
+  }
+}
+
+function applyNavTable(theme) {
+  try {
+    document.getElementById("navth").setAttribute("style", "background-color: " + theme["navtable"] + ";");
+    document.getElementById("navtable").setAttribute("style", "border: 1px solid " + theme["navtable"] + ";");
+  } catch(error) {
+    console.log("Could not get navtable or navth");
+    console.log(error);
+  }
+  try {
+    remainingTags = Array.from(document.getElementsByClassName("navtd"));
+  } catch(error) {
+    console.log("Could not gather navtd tags");
+    console.log(error);
+  }
+  for (item of remainingTags) {
+    try {
+      item.setAttribute("style", "border: 1px solid " + theme["navtable"] + ";");
+    } catch(error) {
+      console.log("Couldn't apply theme property 'navtable'");
+      console.log(error);
+    }
+  }
+}
+
+function applyColorization(theme) {
+  var classesToGet = ["specific", "keyword", "field", "annotation", "string", "comment", "number"];
+  var tagsOfClass;
+  for (item of classesToGet) {
+    try {
+      tagsOfClass = Array.from(document.getElementsByClassName(item));
+    } catch(error) {
+      console.log("Colorization could not gather tagsOfClass");
+      console.log(error);
+    }
+    for (secondItem of tagsOfClass) {
+      try {
+        secondItem.setAttribute("style", "color: " + theme[item] + ";");
+      } catch(error) {
+        console.log("Couldn't apply colorization to '" + secondItem.toString() + "'");
+        console.log(error);
+      }
+    }
+  }
+}
+
+function applyTheme(theme) {
+  applyText(theme);
+  applyBackground(theme);
+  applyLinks(theme);
+  applyNavTable(theme);
+  applyColorization(theme);
+}
+
+function oldApplyTheme(theme) {
+  try { document.body.setAttribute("style", "background-color: " + theme["background"]); }
+  catch { console.log("Couldn't apply theme to body"); }
+  try { var aTags = Array.from(document.getElementsByTagName("a"));
+  aTags.forEach(function(item) {
+    item.setAttribute("style", "color: " + theme["link"]);
+  }); }
+  catch { console.log("Couldn't apply theme to a tags"); }
+  
+  try { var textTags = getTextTags();
+  textTags.forEach(function(item) {
+    item.setAttribute("style", "color: " + theme["text"]);
+  }); }
+  catch { console.log("Couldn't apply theme to text tags"); }
+  
+  try { document.getElementById("navtable").setAttribute("style", "border: 1px solid " + theme["navtable"] + ";"); }
+  catch { console.log("Couldn't apply theme to navtable"); }
+  try { document.getElementById("navth").setAttribute("style", "background-color: " + theme["navtable"] + ";"); }
+  catch { console.log("Couldn't apply theme to navth"); }
+  try { Array.from(document.getElementsByClassName("navtd")).forEach(function(item) {
+    item.setAttribute("style", "border: 1px solid " + theme["navtable"] + "; color: " + theme["text"] + ";");
+  }); }
+  catch { console.log("Couldn't apply theme to class navtd"); }
+  applyColorization(theme);
 }
