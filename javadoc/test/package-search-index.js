ccbee02eed89ea67f1ec68f221dd52b56e79907a
MediumAutomaton <michaelr@wordenhome.net>
2024-01-13 19:27:45 -0600

    1-13-24 nightly - testing JavaDoc; I'm thinking of sharing the libraries we develop here, and it would be good to share JavaDoc for them too

---
+++ javadoc/test/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.firstinspires.ftc.teamcode.fy23.autoexperiment"},{"l":"org.firstinspires.ftc.teamcode.fy23.controls"},{"l":"org.firstinspires.ftc.teamcode.fy23.robot"},{"l":"org.firstinspires.ftc.teamcode.fy23.robot.units"}]
